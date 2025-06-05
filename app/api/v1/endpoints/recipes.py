030f557fcf7a4ba69401a6a941e83a3e541232ea
Kotomi091 <s220102r@st.go.tuat.ac.jp>
2025-06-05 13:30:18 +0900

    acqire_id_and_url_from_db

--- app/api/v1/endpoints/recipes.py
+++ app/api/v1/endpoints/recipes.py
@@ -1,24 +1,3 @@
-
-from fastapi import APIRouter
-from starlette.responses import JSONResponse
-
-router = APIRouter()
-
-
-@router.get("" ,tags=["recipes"])
-def recipes_check():
-    """
-    レシピのチェックエンドポイント
-    """
-    return JSONResponse(content={"dish_name": "生姜焼き"})
-
-@router.get("/{recipe_id}", tags=["recipes"])
-def get_recipe_id(recipe_id: int):
-    """
-    レシピIDを受け取ってIDを返すエンドポイント
-    """
-    return JSONResponse(content={"recipe_id": recipe_id})
-
 from fastapi import APIRouter, Depends, HTTPException
 from sqlalchemy.orm import Session
 from typing import List, Optional
