29caa1dbc2cdfb012453411c4ee0956e85091f4b
CoCo9122 <banaji.yu0517@outlook.jp>
2025-06-09 16:33:01 +0900

    AWSの設定とユーザーのログイン系と登録処理の隠し

--- app/api/v1/endpoints/user.py
+++ app/api/v1/endpoints/user.py
@@ -1,8 +1,10 @@
 from typing import Any
 
 from fastapi import APIRouter, Depends
+from sqlalchemy.orm import Session
 
 from app.api import deps
+from app.crud import user as crud
 from app.schemas import user as schemas
 
 router = APIRouter()
@@ -16,3 +18,15 @@ def read_users_me(current_user = Depends(deps.get_current_user)) -> Any:
     # 返却するカラムを制限
     current_user = schemas.User.from_orm(current_user)
     return current_user
+
+@router.put("/me", response_model=schemas.UserMe)
+def update_user_me(
+    user_update: schemas.UserUpdate,
+    current_user = Depends(deps.get_current_user),
+    db: Session = Depends(deps.get_db)
+) -> Any:
+    """
+    現在ログインしているユーザー情報を更新
+    """
+    updated_user = crud.update(db=db, db_obj=current_user, obj_in=user_update)
+    return schemas.UserMe.from_orm(updated_user)
